5808bdf3fb2fad43cb507e56b9b9fbbac89399a3
palliotte <114391946+palliotte@users.noreply.github.com>
2022-10-21 20:00:04 +0800

    Restore '10/21/2022, 7:57:00 PM Update pxt.json, main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,1 +1,52 @@
-
+enum RadioMessage {
+    message1 = 49434,
+    touché = 2826
+}
+radio.onReceivedMessage(RadioMessage.touché, function () {
+    game.removeLife(1)
+})
+input.onButtonPressed(Button.A, function () {
+    sprite.change(LedSpriteProperty.X, -1)
+    radio.sendValue("enn x", sprite.get(LedSpriteProperty.X))
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendValue("enn bul", bullet.get(LedSpriteProperty.Y))
+    bullet = game.createSprite(sprite.get(LedSpriteProperty.X), 4)
+    for (let index = 0; index < 5; index++) {
+        bullet.change(LedSpriteProperty.Y, -1)
+        basic.pause(200)
+        if (bullet.isTouching(ennemie)) {
+            bullet.delete()
+            radio.sendMessage(RadioMessage.touché)
+        }
+    }
+    bullet.delete()
+})
+input.onButtonPressed(Button.B, function () {
+    sprite.change(LedSpriteProperty.X, 1)
+    radio.sendValue("enn x", sprite.get(LedSpriteProperty.X))
+})
+radio.onReceivedValue(function (name, value) {
+    if (name == "enn x") {
+        ennemie.set(LedSpriteProperty.X, value)
+    }
+    if (name == "enn bul") {
+        ennemie_bullet = game.createSprite(ennemie.get(LedSpriteProperty.X), 4)
+        for (let index = 0; index < 5; index++) {
+            ennemie_bullet.change(LedSpriteProperty.Y, -1)
+            basic.pause(200)
+            if (ennemie_bullet.isTouching(sprite)) {
+                ennemie_bullet.delete()
+            }
+        }
+        ennemie_bullet.delete()
+    }
+})
+let ennemie_bullet: game.LedSprite = null
+let bullet: game.LedSprite = null
+let ennemie: game.LedSprite = null
+let sprite: game.LedSprite = null
+radio.setGroup(125)
+sprite = game.createSprite(2, 4)
+ennemie = game.createSprite(2, 0)
+game.setLife(3)

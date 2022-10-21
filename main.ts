95c8e62f8f87b4844bfe5bd13f7e140dc3f8f83d
palliotte <114391946+palliotte@users.noreply.github.com>
2022-10-21 20:08:17 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,23 +2,13 @@ enum RadioMessage {
     message1 = 49434,
     touché = 2826
 }
-function Enn_bul () {
-    ennemie_bullet = game.createSprite(ennemie.get(LedSpriteProperty.X), 4)
-    for (let index = 0; index < 5; index++) {
-        ennemie_bullet.change(LedSpriteProperty.Y, -1)
-        basic.pause(200)
-        if (ennemie_bullet.isTouching(sprite)) {
-            ennemie_bullet.delete()
-        }
+radio.onReceivedValue(function (name, value) {
+    if (name == "enn x") {
+        ennemie.set(LedSpriteProperty.X, value)
     }
-    ennemie_bullet.delete()
-}
-radio.onReceivedMessage(RadioMessage.touché, function () {
-    game.removeLife(1)
-})
-input.onButtonPressed(Button.A, function () {
-    sprite.change(LedSpriteProperty.X, -1)
-    radio.sendValue("enn x", sprite.get(LedSpriteProperty.X))
+    if (name == "enn bul") {
+        Enn_bul()
+    }
 })
 input.onButtonPressed(Button.AB, function () {
     radio.sendValue("enn bul", bullet.get(LedSpriteProperty.Y))
@@ -33,20 +23,30 @@ input.onButtonPressed(Button.AB, function () {
     }
     bullet.delete()
 })
+function Enn_bul () {
+    ennemie_bullet = game.createSprite(ennemie.get(LedSpriteProperty.X), 4)
+    for (let index = 0; index < 5; index++) {
+        ennemie_bullet.change(LedSpriteProperty.Y, -1)
+        basic.pause(200)
+        if (ennemie_bullet.isTouching(sprite)) {
+            ennemie_bullet.delete()
+        }
+    }
+    ennemie_bullet.delete()
+}
+input.onButtonPressed(Button.A, function () {
+    sprite.change(LedSpriteProperty.X, -1)
+    radio.sendValue("enn x", sprite.get(LedSpriteProperty.X))
+})
 input.onButtonPressed(Button.B, function () {
     sprite.change(LedSpriteProperty.X, 1)
     radio.sendValue("enn x", sprite.get(LedSpriteProperty.X))
 })
-radio.onReceivedValue(function (name, value) {
-    if (name == "enn x") {
-        ennemie.set(LedSpriteProperty.X, value)
-    }
-    if (name == "enn bul") {
-        Enn_bul()
-    }
+radio.onReceivedMessage(RadioMessage.touché, function () {
+    game.removeLife(1)
 })
-let bullet: game.LedSprite = null
 let ennemie_bullet: game.LedSprite = null
+let bullet: game.LedSprite = null
 let ennemie: game.LedSprite = null
 let sprite: game.LedSprite = null
 radio.setGroup(125)

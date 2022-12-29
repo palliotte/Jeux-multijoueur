2cf379c36af0d1d76005ba5b567a3cafc3e91fd2
palliotte <114391946+palliotte@users.noreply.github.com>
2022-12-29 17:36:21 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,36 +1,19 @@
 enum RadioMessage {
     message1 = 49434,
-    touché = 2826
+    touché = 2826,
+    GAGNER = 52909
 }
 input.onButtonPressed(Button.A, function () {
-    sprite.change(LedSpriteProperty.X, -1)
-    radio.sendValue("enn x", sprite.get(LedSpriteProperty.X))
+    if (!(VIE == 0)) {
+        sprite.change(LedSpriteProperty.X, -1)
+        radio.sendValue("enn x", sprite.get(LedSpriteProperty.X))
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    sprite.change(LedSpriteProperty.X, 1)
-    radio.sendValue("enn x", sprite.get(LedSpriteProperty.X))
-})
-radio.onReceivedMessage(RadioMessage.touché, function () {
-    game.removeLife(1)
-})
-input.onButtonPressed(Button.AB, function () {
-    bullet = game.createSprite(sprite.get(LedSpriteProperty.X), 4)
-    radio.sendValue("enn bul", bullet.get(LedSpriteProperty.X))
-    for (let index = 0; index < 5; index++) {
-        bullet.change(LedSpriteProperty.Y, -1)
-        basic.pause(200)
-        if (bullet.isTouching(ennemie)) {
-            bullet.delete()
-            radio.sendMessage(RadioMessage.touché)
-        }
-        if (ennemie_bullet) {
-            if (bullet.isTouching(ennemie_bullet)) {
-                ennemie_bullet.delete()
-                bullet.delete()
-            }
-        }
+    if (!(VIE == 0)) {
+        sprite.change(LedSpriteProperty.X, 1)
+        radio.sendValue("enn x", sprite.get(LedSpriteProperty.X))
     }
-    bullet.delete()
 })
 radio.onReceivedValue(function (name, value) {
     if (name == "enn x") {
@@ -54,11 +37,81 @@ radio.onReceivedValue(function (name, value) {
         ennemie_bullet.delete()
     }
 })
-let ennemie_bullet: game.LedSprite = null
+radio.onReceivedMessage(RadioMessage.GAGNER, function () {
+    sprite.delete()
+    ennemie.delete()
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            # # # # #
+            # # . # #
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+        basic.pause(100)
+        basic.showString("SCORE=" + SCORE)
+    }
+    control.reset()
+})
+input.onButtonPressed(Button.AB, function () {
+    if (!(VIE == 0)) {
+        bullet = game.createSprite(sprite.get(LedSpriteProperty.X), 4)
+        radio.sendValue("enn bul", bullet.get(LedSpriteProperty.X))
+        for (let index = 0; index < 5; index++) {
+            bullet.change(LedSpriteProperty.Y, -1)
+            basic.pause(200)
+            if (bullet.isTouching(ennemie)) {
+                bullet.delete()
+                radio.sendMessage(RadioMessage.touché)
+            }
+            if (ennemie_bullet) {
+                if (bullet.isTouching(ennemie_bullet)) {
+                    ennemie_bullet.delete()
+                    bullet.delete()
+                }
+            }
+        }
+        bullet.delete()
+    }
+})
+radio.onReceivedMessage(RadioMessage.touché, function () {
+    VIE += -1
+})
 let bullet: game.LedSprite = null
+let ennemie_bullet: game.LedSprite = null
+let SCORE = 0
+let VIE = 0
 let ennemie: game.LedSprite = null
 let sprite: game.LedSprite = null
 radio.setGroup(125)
 sprite = game.createSprite(2, 4)
 ennemie = game.createSprite(2, 0)
-game.setLife(3)
+VIE = 3
+SCORE = 0
+basic.forever(function () {
+    if (!(VIE == 0)) {
+        radio.sendValue("enn x", sprite.get(LedSpriteProperty.X))
+    }
+    if (VIE == 0) {
+        radio.sendMessage(RadioMessage.GAGNER)
+        sprite.delete()
+        ennemie.delete()
+        for (let index = 0; index < 5; index++) {
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . . . .
+                . # # # .
+                # . . . #
+                `)
+            basic.pause(100)
+            basic.showString("SCORE=" + SCORE)
+        }
+        control.reset()
+    }
+})
+loops.everyInterval(1000, function () {
+    if (!(VIE == 0)) {
+        SCORE += 1
+    }
+})
